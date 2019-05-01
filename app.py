492300547ffe8657612a69812603d6e309fc05f9
Theo Hermitte <theoh1101@gmail.com>
2019-05-01 02:37:34 +0200

    add the possibility to change the language

--- app.py
+++ app.py
@@ -1,18 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    data = json.loads(
+def get_language():
+    lang = request.cookies.get("language")
+    if lang not in ["en", "fr"]:
+        lang = "en"
+
+    return json.load(
         open(
-            "lang/en.lang"
-        ).read()
+            "lang/{}.lang".format(
+                lang
+            )
+        )
     )
 
-    return render_template('home.html', lang=data)
+
+@app.route('/')
+def index():
+    res = make_response(render_template("home.html", lang=get_language()))
+    res.set_cookie('last', 'home.html')
+    return res
+
+
+@app.route('/set_<lang>')
+def set_lang(lang):
+    last = request.cookies.get("last")
+    if last is None:
+        last = "home.html"
+
+    if lang in ["en", "fr"]:
+        data = json.load(
+            open(
+                "lang/{}.lang".format(
+                    lang
+                )
+            )
+        )
+    else:
+        data = get_language()
+
+    res = make_response(render_template(last, lang=data))
+    res.set_cookie('language', lang)
+    return res
 
 
 if __name__ == '__main__':
